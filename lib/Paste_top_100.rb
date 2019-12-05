2b828a9a722b7e01c13427efdae796a3f17c8409
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-05 14:16:07 +0000

    first commit

---
+++ lib/Paste_top_100.rb
@@ -0,0 +1,6 @@
+require "Paste_top_100/version"
+
+module PasteTop100
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Paste_top_100/version.rb
@@ -0,0 +1,3 @@
+module PasteTop100
+  VERSION = "0.1.0"
+end
