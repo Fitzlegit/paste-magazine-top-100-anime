e7926c3a4a784264b7d9026cb28b439586beb305
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-05 20:34:12 +0000

    updates to anime.rb

---
+++ lib/Paste_top_100/anime.rb
@@ -0,0 +1,18 @@
+class PasteTop100::Anime
+
+  attr_accessor :rank, :title, :release_date, :synopsis, :director, :gross_earnings, :movie_rating, :runtime, :genre
+
+  @@all = [] #=> stores all movie objects into an array
+
+  def self.all
+    @@all
+  end
+
+  
+
+
+
+
+
+
+end
